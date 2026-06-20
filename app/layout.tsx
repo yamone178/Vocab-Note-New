d52fed442955c25909c1d90bd2dd3da6c85223dc
yamone-lom <yamone@lomtech.net>
2026-06-20 22:59:26 +0630

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
